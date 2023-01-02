361afc88f512579f37e549db109bec229f49e503
Abdallah Mehiz <abdallahmehiz@hotmail.com>
2023-01-02 16:54:09 +0100

    Change Port and host locally

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    host: true,
+    port: 6969,
+  },
   plugins: [vue()],
-})
+});
