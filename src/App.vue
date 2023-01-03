23ee6d956da839d157481d48854ac6d5352dbc50
Abdallah Mehiz <abdallahmehiz@hotmail.com>
2023-01-03 11:23:58 +0100

    Make the app a grid add navigation bar with little styling

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
-<script setup></script>
+<script setup>
+import NavigationBar from "./components/NavigationBar.vue";
+</script>
 
 <template>
+  <NavigationBar />
   <h1>hello world</h1>
 </template>
 
---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <nav class="navbar">
+    <div class="button">
+      <router-link to="/blog">Blog</router-link>
+    </div>
+    <div class="button">
+      <router-link to="/Projects">Projects</router-link>
+    </div>
+    <div class="button">
+      <router-link to="/Resume">Resume</router-link>
+    </div>
+    <div class="button">
+      <router-link to="/Contact">Contact</router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "NavigationBar",
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style src="../style/NavigationBar.css"></style>
