042f06dc85b362c3dcccd3df8f4218950d1bd5f6
Abdallah Mehiz <abdallahmehiz@hotmail.com>
2023-01-03 12:44:56 +0100

    forgot router view

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
 import NavigationBar from "./components/NavigationBar.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <NavigationBar />
   <h1>hello world</h1>
+  <RouterView />
 </template>
 
 <style scoped></style>
