d95ce24a664c0ae15eb43f1eba83c6815996cadc
Abdallah Mehiz <abdallahmehiz@hotmail.com>
2023-01-04 13:09:49 +0100

    Make Pattern a seperate vue component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <script setup>
 import NavigationBar from "./components/NavigationBar.vue";
+import Pattern from "./components/Pattern.vue";
 import ContactView from "./views/ContactView.vue";
 </script>
 
 <template>
   <NavigationBar class="navbar" />
+  <Pattern class="pattern" />
   <ContactView />
 </template>
 
-<style scoped></style>
+<style scoped>
+.pattern {
+  grid-area: pattern;
+  border-top: 2px solid #4d493e;
+}
+</style>
