4042a136553b945b52cc4cb313dd5e11705e8eec
Abdallah Mehiz <54363735+AbdallahMehiz@users.noreply.github.com>
2023-01-17 05:28:30 +0100

    Revert "Switch to sass"
    This reverts commit 23d154d8d4fac98b2df1456c922400002c31a832.

--- src/components/SocialComponent.vue
+++ src/components/SocialComponent.vue
@@ -25,46 +25,72 @@ export default {
 };
 </script>
 
-<style lang="sass" scoped>
-a
- text-decoration: none
-
-.socials-icons
-  margin: 0
-svg
-  width: 80px
-  height: 80px
-  fill: #3f3b33
-  transition: 600ms
-  align-self: center !important
-figure:hover
-  background-color: #3f3b33
-figure:hover
-  .socials-icons svg
-    fill: white
-  figcaption
-    transition: 100ms !important
-    font-weight: 600
-figure
-  height: 120px
-  width: 100%
-  align-items: center
-  justify-content: space-between
-  display: flex
-  flex-direction: column
-  margin: 0
-  background-color: #dcd8c0
-  transition: 600ms
-  padding-top: 0.3rem
-  :not(figcaption)
-    margin: 0rem
-  figcaption
-    order: 4
-    width: 100%
-    padding: 0.2rem, 0rem
-    text-align: center
-    font-size: 1.2rem
-    background-color: #3f3b33
-    color: #bab5a1
-    transition: 600ms
+<style scoped>
+a {
+  text-decoration: none;
+}
+.socials-icons {
+  margin: 0;
+}
+svg {
+  width: 80px;
+  height: 80px;
+  fill: #3f3b33;
+  transition: 600ms;
+  align-self: center !important;
+}
+figure:hover {
+  background-color: #3f3b33;
+}
+figure:hover .socials-icons svg {
+  fill: white;
+}
+figure:hover figcaption {
+  transition: 100ms !important;
+  font-weight: 600;
+}
+img {
+  max-width: inherit;
+  max-height: inherit;
+}
+figure {
+  height: 120px;
+  width: 100%;
+  align-items: center;
+  justify-content: space-between;
+  display: -webkit-box;
+  display: -moz-box;
+  display: -webkit-flex;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -moz-box-orient: vertical;
+  -o-box-orient: vertical;
+  -webkit-flex-direction: column;
+  -ms-flex-direction: column;
+  flex-direction: column;
+  margin: 0;
+  background-color: #dcd8c0;
+  transition: 600ms;
+  padding-top: 0.3rem;
+}
+figure > :not(figcaption) {
+  margin: 0rem;
+  transition: 600ms;
+}
+figure > figcaption {
+  -webkit-box-ordinal-group: -1;
+  -moz-box-ordinal-group: -1;
+  -o-box-ordinal-group: -1;
+  -ms-flex-order: -1;
+  -webkit-order: -1;
+  order: 4;
+  width: 100%;
+  padding: 0.2rem, 0rem;
+  text-align: center;
+  font-size: 1.2rem;
+  background-color: #3f3b33;
+  color: #bab5a1;
+  transition: 600ms;
+}
 </style>
