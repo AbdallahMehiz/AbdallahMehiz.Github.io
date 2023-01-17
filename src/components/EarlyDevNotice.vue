23d154d8d4fac98b2df1456c922400002c31a832
Abdallah Mehiz <AbdallahMehiz@hotmail.com>
2023-01-17 05:07:45 +0100

    Switch to sass

--- src/components/EarlyDevNotice.vue
+++ src/components/EarlyDevNotice.vue
@@ -24,40 +24,20 @@ export default {
 };
 </script>
 
-<style scoped>
-figure {
-  display: -webkit-box;
-  display: -moz-box;
-  display: -webkit-flex;
-  display: -ms-flexbox;
-  display: box;
-  display: flex;
-  -webkit-box-orient: vertical;
-  -moz-box-orient: vertical;
-  -o-box-orient: vertical;
-  -webkit-flex-direction: column;
-  -ms-flex-direction: column;
-  flex-direction: column;
-  margin: 0;
-  margin-bottom: 1rem;
-  background-color: #dcd8c0;
-}
-figure > :not(figcaption) {
-  margin: 0.5rem;
-}
-figure > figcaption {
-  -webkit-box-ordinal-group: -1;
-  -moz-box-ordinal-group: -1;
-  -o-box-ordinal-group: -1;
-  -ms-flex-order: -1;
-  -webkit-order: -1;
-  order: -1;
-  padding: 0.3rem 0.5rem 0rem 0.5rem;
-  font-size: 1.2rem;
-  background-color: #a8a487;
-  color: #454138;
-}
-iframe {
-  border: 0.1rem solid #bab5a1;
-}
+<style lang="sass" scoped>
+figure
+  display: box
+  display: flex
+  flex-direction: column
+  margin: 0
+  margin-bottom: 1rem
+  background-color: #dcd8c0
+  :not(figcaption)
+    margin: 0.5rem
+  figcaption
+    order: -1
+    padding: 0.3rem 0.5rem 0rem 0.5rem
+    font-size: 1.2rem
+    background-color: #a8a487
+    color: #454138
 </style>
--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -29,164 +29,121 @@ export default {
 };
 </script>
 
-<style scoped>
-button:hover .icon {
-  background-color: #dcd8c0;
-  display: inline-block;
-}
-.icon {
-  background-color: #454138;
-  display: inline-block;
-  height: 15px;
-  width: 15px;
-  min-height: 15px;
-  min-width: 15px;
-  transition: 300ms;
-}
-nav {
-  margin-top: 10px;
-  margin-bottom: 10px;
-  display: flex;
-  flex-direction: row;
-  align-items: left;
-  grid-area: nav;
-  color: white;
-  padding-left: 2.5rem;
-  background-image: linear-gradient(
-    90deg,
-    rgba(77, 73, 62, 0.2),
-    rgba(77, 73, 62, 0.2) 14px,
-    transparent 14px,
-    transparent 20px,
-    rgba(77, 73, 62, 0.2) 20px,
-    rgba(77, 73, 62, 0.2) 23px,
-    transparent 23px
-  );
-}
-
-button {
-  min-width: 120px;
-  text-align: left;
-}
-
-/* css code of navigation bar items from
-https://github.com/metakirby5/yorha
-tysm
-*/
-
+<style lang="sass" scoped>
+button
+  min-width: 120px
+  text-align: left
+  &:hover
+    .icon
+      background-color: #dcd8c0
+      display: inline-block
+.icon
+  background-color: #454138
+  display: inline-block
+  height: 15px
+  width: 15px
+  min-height: 15px
+  min-width: 15px
+  transition: 300ms
+nav
+  margin-top: 10px
+  margin-bottom: 10px
+  display: flex
+  flex-direction: row
+  align-items: left
+  grid-area: nav
+  color: white
+  padding-left: 2.5rem
+  background-image: linear-gradient(90deg,rgba(77, 73, 62, 0.2),rgba(77, 73, 62, 0.2) 14px,transparent 14px,transparent 20px,rgba(77, 73, 62, 0.2) 20px,rgba(77, 73, 62, 0.2) 23px,transparent 23px)
 input:disabled,
 button:disabled,
-.button-disabled {
-  padding: 0.5rem;
-  font-size: 1em;
-  border: none;
-  outline: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  border-radius: 0;
-  background-color: #dcd8c0;
-  color: inherit;
-  font-family: inherit;
-  letter-spacing: inherit;
-  font-weight: inherit;
-  color: #bab5a1;
-  cursor: not-allowed;
-  text-decoration: none;
-}
+.button-disabled
+  padding: 0.5rem
+  font-size: 1em
+  border: none
+  outline: none
+  -webkit-appearance: none
+  -moz-appearance: none
+  appearance: none
+  border-radius: 0
+  background-color: #dcd8c0
+  color: inherit
+  font-family: inherit
+  letter-spacing: inherit
+  font-weight: inherit
+  color: #bab5a1
+  cursor: not-allowed
+  text-decoration: none
 button:not(:disabled),
-.button {
-  max-height: 30px;
-  padding: 0rem 0.5rem 0.3rem 0.5rem;
-  font-size: 1.3em;
-  border: none;
-  outline: none;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-  appearance: none;
-  border-radius: 0;
-  background-color: #bab5a1;
-  color: rgb(63, 59, 51);
-  font-family: inherit;
-  letter-spacing: inherit;
-  font-weight: inherit;
-  cursor: pointer;
-  -webkit-transition-duration: 0.2s;
-  -moz-transition-duration: 0.2s;
-  -o-transition-duration: 0.2s;
-  -ms-transition-duration: 0.2s;
-  transition-duration: 0.2s;
-  -webkit-transition-property: color, background-color, box-shadow;
-  -moz-transition-property: color, background-color, box-shadow;
-  -o-transition-property: color, background-color, box-shadow;
-  -ms-transition-property: color, background-color, box-shadow;
-  transition-property: color, background-color, box-shadow;
-  position: relative;
-  z-index: 1;
-}
+.button
+  max-height: 30px
+  padding: 0rem 0.5rem 0.3rem 0.5rem
+  font-size: 1.3em
+  border: none
+  outline: none
+  -webkit-appearance: none
+  -moz-appearance: none
+  appearance: none
+  border-radius: 0
+  background-color: #bab5a1
+  color: rgb(63, 59, 51)
+  font-family: inherit
+  letter-spacing: inherit
+  font-weight: inherit
+  cursor: pointer
+  -webkit-transition-duration: 0.2s
+  -moz-transition-duration: 0.2s
+  -o-transition-duration: 0.2s
+  -ms-transition-duration: 0.2s
+  transition-duration: 0.2s
+  transition-property: color, background-color, box-shadow
+  position: relative
+  z-index: 1
 button:not(:disabled):hover,
-.button:hover {
-  -webkit-box-shadow: 0.2em 0.2em 0.1em 0 #bab5a1;
-  box-shadow: 0.2em 0.2em 0.1em 0 #bab5a1;
-}
+.button:hover
+  -webkit-box-shadow: 0.2em 0.2em 0.1em 0 #bab5a1
+  box-shadow: 0.2em 0.2em 0.1em 0 #bab5a1
+  background-color: transparent
+  color: #dcd8c0
 button:not(:disabled):before,
-.button:before {
-  content: "";
-  -webkit-transition: all 0.2s;
-  -moz-transition: all 0.2s;
-  -o-transition: all 0.2s;
-  -ms-transition: all 0.2s;
-  transition: all 0.2s;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-}
+.button:before
+  content: ""
+  transition: all 0.2s
+  position: absolute
+  top: 0
+  bottom: 0
+  left: 0
+  right: 0
 button:not(:disabled):after,
-.button:after {
-  content: "";
-  -webkit-transition: all 0.2s;
-  -moz-transition: all 0.2s;
-  -o-transition: all 0.2s;
-  -ms-transition: all 0.2s;
-  transition: all 0.2s;
-  -webkit-transition-timing-function: ease-out;
-  -moz-transition-timing-function: ease-out;
-  -o-transition-timing-function: ease-out;
-  -ms-transition-timing-function: ease-out;
-  transition-timing-function: ease-out;
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  width: 0;
-  background-color: #454138;
-  z-index: -1;
-}
-button:not(:disabled):hover,
-.button:hover {
-  background-color: transparent;
-  color: #dcd8c0;
-}
+.button:after
+  content: ""
+  -webkit-transition: all 0.2s
+  -moz-transition: all 0.2s
+  -o-transition: all 0.2s
+  -ms-transition: all 0.2s
+  transition: all 0.2s
+  transition-timing-function: ease-out
+  position: absolute
+  top: 0
+  bottom: 0
+  left: 0
+  width: 0
+  background-color: #454138
+  z-index: -1
 button:not(:disabled):hover:before,
-.button:hover:before {
-  top: -0.2rem;
-  bottom: -0.2rem;
-  border: solid #454138;
-  border-width: 0.1rem 0;
-}
+.button:hover:before
+  top: -0.2rem
+  bottom: -0.2rem
+  border: solid #454138
+  border-width: 0.1rem 0
 button:not(:disabled):hover:after,
-.button:hover:after {
-  width: 100%;
-}
+.button:hover:after
+  width: 100%
 button:not(:disabled):active,
-.button:active {
-  color: #454138;
-  color: white;
-}
+.button:active
+  color: #454138
+  color: white
 button:not(:disabled):active:after,
-.button:active:after {
-  background-color: #dcd8c0;
-}
+.button:active:after
+  background-color: #dcd8c0
 </style>
