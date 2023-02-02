4eff7004f1132c44197cddb7907eea6b167b3259
Abdallah Mehiz <AbdallahMehiz@hotmail.com>
2023-02-02 23:50:35 +0100

    Use google drive for the resume preview my ego was hurt and i hate algeria just a bit more

--- src/views/ResumeView.vue
+++ src/views/ResumeView.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="resume-view">
     <h1 class="title">Resume</h1>
-    <div class="document-container">
-      <vue-pdf-embed class="document" source="../../assets/Resume.pdf" />
-    </div>
+    <iframe
+      class="document"
+      frameborder="0"
+      src="https://drive.google.com/file/d/1-y8cei8J7xN62Q_wNpuLgdLs7ZmeQNjm/preview"
+      allow="autoplay"
+    ></iframe>
   </div>
 </template>
 
@@ -12,44 +15,19 @@ import VuePdfEmbed from "vue-pdf-embed";
 
 export default {
   name: "ResumeView",
-  setup() {
-    return {};
-  },
   components: { VuePdfEmbed },
 };
 </script>
 
 <style scoped>
 body {
-  overflow: scroll !important;
 }
 .resume-view {
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: auto;
-  grid-template-areas:
-    "title ..."
-    "resume resume"
-    "resume resume"
-    "resume resume"
-    "resume resume"
-    "resume resume";
+  display: flex;
+  flex-direction: column;
 }
 .title {
   grid-area: title;
-}
-.document-container {
-  grid-area: resume;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: auto;
-  grid-template-areas:
-    "resume resume"
-    "resume resume"
-    "resume resume"
-    "resume resume"
-    "resume resume"
-    "resume resume";
 }
 .document {
   grid-area: resume;
