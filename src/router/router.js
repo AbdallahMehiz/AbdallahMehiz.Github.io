9b60379571dcfb22979823773b1468aded09e419
Abdallah Mehiz <AbdallahMehiz@hotmail.com>
2023-02-04 03:16:41 +0100

    Add feed to to nav bar

--- src/router/router.js
+++ src/router/router.js
@@ -18,6 +18,11 @@ const router = createRouter({
       name: "home",
       component: () => import("../views/HomeView.vue"),
     },
+    {
+      path: "/feed",
+      name: "feed",
+      component: () => import("../views/FeedView.vue"),
+    },
   ],
 });
 
