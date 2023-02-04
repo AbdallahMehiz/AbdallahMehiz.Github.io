edb82f41faa0b8bf13f726bb77caf24ff2d586f3
Abdallah Mehiz <AbdallahMehiz@hotmail.com>
2023-02-04 03:31:15 +0100

    Add 404 Page not found view

---
+++ src/views/NotFoundView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="not-found-view">
+    <h1 class="title">404</h1>
+    <h2 class="subtitle">Page not found</h2>
+    <p class="description">
+      The page you are looking for does not exist. Please check the URL and try
+      again.
+    </p>
+  </div>
+</template>
