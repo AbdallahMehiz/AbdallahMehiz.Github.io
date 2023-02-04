730c4d11bbe5e804999e381aff9e40606532d5ba
AbdallahMehiz <AbdallahMehiz@users.noreply.github.com>
2023-02-04 14:21:13 +0000

    deploy: 7f4456b95f6013e99a1b0798bed7a014bfcebd1f

--- assets/FeedView-82a6c1ef.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as o,b as t}from"./index-da0ab578.js";const s={},n=t("h1",null,"Feed",-1),_=[n];function r(a,d){return c(),o("div",null,_)}const i=e(s,[["render",r]]);export{i as default};
