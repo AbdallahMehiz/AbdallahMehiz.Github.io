713d9a46825ca92b3a659cd7d4072172026f67e4
AbdallahMehiz <AbdallahMehiz@users.noreply.github.com>
2023-02-09 18:06:10 +0000

    deploy: 6f6310c584498e441c5764f861658845debb1d89

--- assets/MediaView-d93fd87a.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"./index-6c1bf188.js";const o={};function r(t,a){return c(),n("h1",null,"Media")}const _=e(o,[["render",r]]);export{_ as default};
---
+++ assets/ProjectsView-bbaea71c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-297d6c38.js";const r={};function t(n,s){return c(),o("h1",null,"Projects")}const a=e(r,[["render",t]]);export{a as default};
