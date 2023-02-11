1eae38be9824b65369b976bead56b3c603b20ecc
AbdallahMehiz <AbdallahMehiz@users.noreply.github.com>
2023-02-11 19:10:51 +0000

    deploy: 5582ba207ea52aef998f1cad7d1cb3852c40a531

---
+++ assets/ProjectsView-6a37e766.js
@@ -0,0 +1,1 @@
+import{_ as t,o as r,c as s,t as c}from"./index-5fd8c106.js";const o={};function a(e,n){return r(),s("h1",null,c(e.$t("message.projects.header")),1)}const l=t(o,[["render",a]]);export{l as default};
